47d751963c7bfb5668d7e939e517b2ac170a6520
邢汉杰 <xinghanjie@picc.com.cn>
2024-10-14 17:08:07 +0800

    feat: init

---
+++ Example/Example/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Example
+//
+//  Created by xinghanjie on 2024/10/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
